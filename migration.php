7b669fd9268df2b00502dfa43fa1119053b3f4da
guilhermeecg <guilherme@ecg.inf.br>
2025-03-02 18:13:51 -0300

    faz migrations serem mais dinamicas, podendo adicionar os schemas em um array

--- migration.php
+++ migration.php
@@ -6,16 +6,35 @@ require_once __DIR__ . "/eloquent.php";
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Schema\Blueprint;
 
-$tableName = "tb_url";
+$tableUrl = "tb_url";
 
-Capsule::schema()->dropIfExists($tableName);
+$tables = [
+    [
+        "name" => "tb_url",
+        "schema" => function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("short_code")->unique();
+            $table->text("original_url");
+            $table->dateTime("expiration_date");
+            $table->timestamps();
+        }
+    ],
+    [
+        "name" => "tb_qrcode",
+        "schema" => function (Blueprint $table) {
+            $table->increments("id");
+            $table->text("qrcode");
+            $table->text("original_url");
+            $table->timestamps();
+        }
+    ]
+];
 
-Capsule::schema()->create($tableName, function (Blueprint $table) {
-    $table->increments("id");
-    $table->string("short_code")->unique();
-    $table->text("original_url");
-    $table->dateTime("expiration_date");
-    $table->timestamps();
-});
+$tableList = [];
+foreach ($tables as $table) {
+    $tableList[] = $table["name"];
+    Capsule::schema()->dropIfExists($table["name"]);
+    Capsule::schema()->create($table["name"], $table["schema"]);
+}
 
-echo "created table: " . $tableName . "\n";
+echo "created table(s): " . implode(", ", $tableList) . "\n";
